6b542408494201b73fd8bb0122e4b62d0891bf68
Victor Cheruiyot <68907415+VictorC99@users.noreply.github.com>
2025-02-19 09:59:08 +0300

    Update wsgi.py

--- dairymanagement/wsgi.py
+++ dairymanagement/wsgi.py
@@ -1,1 +1,21 @@
+"""
+WSGI config for dairy_management project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+import sys
+
+# Add the project’s path to the sys.path
+sys.path.append('/opt/render/project/src')
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dairy_management.settings')
+
+application = get_wsgi_application()
